40d417b96ec54390f19f46833210da3abbb6ad6a
user <skylerbala@users-macbook-pro.local>
2017-08-22 10:45:48 -0700

    Initial Commit

---
+++ LlamaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LlamaApp
+//
+//  Created by Skyler Bala on 8/22/17.
+//  Copyright © 2017 Skyler Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
